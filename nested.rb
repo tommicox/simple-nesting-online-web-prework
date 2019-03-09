434886fdf5b41ad96f6847e4ba4178d3c84ce260
Tommi Cox <tommicox@gmail.com>
2019-03-09 18:08:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash [:dennis_ritchies]{:languages]
+     programmer_hash {[:dennis_ritchies]{:languages]}
 end
 
 def adding_matz
