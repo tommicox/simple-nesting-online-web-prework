07b08d1d70fe3e38fb22760a5fa52b2dbd1c9c64
Tommi Cox <tommicox@gmail.com>
2019-03-09 18:12:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+programmer_hash =
+   {
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
 end
 
 def changing_alan
